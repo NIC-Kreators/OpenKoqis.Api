3499e7837f6645d089412955d5142e60ee9c53f9
Andrey Kirik <dev.andrey.kirik@gmail.com>
2026-07-28 23:56:17 +0500

    feat: add mongo and mqtt into the AppHost

--- src/OpenKoqis.Api.AppHost/AppHost.cs
+++ src/OpenKoqis.Api.AppHost/AppHost.cs
@@ -1,5 +1,26 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.OpenKoqis_Api>("openkoqis-api");
+// Main Database
+var mongoUsername = builder.AddParameter("mongo-username", value: "develop_admin");
+var mongoPassword = builder.AddParameter("mongo-password", secret: true);
+
+var mongo = builder
+    .AddMongoDB("openkoqis-mongo", userName: mongoUsername, password: mongoPassword)
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+var mongoDb = mongo.AddDatabase("openkoqis");
+
+// Message Bus with IoT devices (mqtt via eclipse-mosquitto). Maybe it should be a NuGet package or use NATS
+var messageBus = builder
+    .AddContainer("openkoqis-mqtt", "eclipse-mosquitto")
+    .WithEndpoint(name: "mqtt", port: 1883, targetPort: 1883)
+    .WithBindMount("../../config/mosquitto/config", "/mosquitto/config")
+    .WithBindMount("../../config/mosquitto/logs", "/mosquitto/logs")
+    .WithBindMount("../../config/mosquitto/data", "/mosquitto/data")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+builder.AddProject<Projects.OpenKoqis_Api>("openkoqis-api")
+    .WithReference(mongoDb)
+    .WithReference(messageBus.GetEndpoint("mqtt"));
 
 builder.Build().Run();
