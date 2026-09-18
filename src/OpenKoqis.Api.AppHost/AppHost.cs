962ee497e36f6168a484fd5d6c77d7f517a9d8f1
Andrey Kirik <dev.andrey.kirik@gmail.com>
2026-09-18 17:38:16 +0500

    refactor: rewrite Mongo registration
    Make it with an extension method, so it can fit the "chain" in the `AppHost`

--- src/OpenKoqis.Api.AppHost/AppHost.cs
+++ src/OpenKoqis.Api.AppHost/AppHost.cs
@@ -2,13 +2,12 @@ using OpenKoqis.Api.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mongoDb = Mongo.GetMongo(builder);
 var postgres = Postgres.GetPostgres(builder);
 var messageBusEndpoint = MessageBus.GetMessageBus(builder);
 var keycloak = builder.AddKeycloak("keycloak");
 
 builder.AddProject<Projects.OpenKoqis_Api>("openkoqis-api")
-    .WithReference(mongoDb)
+    .WithMongo(builder)
     .WithReference(postgres)
     .WithReference(messageBusEndpoint)
     .WithReference(keycloak);
--- src/OpenKoqis.Api.AppHost/Mongo.cs
+++ src/OpenKoqis.Api.AppHost/Mongo.cs
@@ -2,20 +2,35 @@ namespace OpenKoqis.Api.AppHost;
 
 public static class Mongo
 {
-    public static IResourceBuilder<MongoDBDatabaseResource> GetMongo(IDistributedApplicationBuilder builder)
+    /// <summary>
+    /// Injects a MongoDB resource for development.
+    /// <para>
+    /// Purpose of the service: General DB for OpenKoqis.
+    /// It was developed before "Modular" separation, so it's not pinned to any module.
+    /// May be deleted if no module won't need for it. Left for old non-modular code.
+    /// </para>
+    /// </summary>
+    /// <param name="resourceBuilder">The resource where connection string will be injected.</param>
+    /// <param name="appBuilder">Application builder to create new Aspire resources.</param>
+    /// <typeparam name="TDestination">The destination resource.</typeparam>
+    /// <returns>The <see cref="IResourceBuilder{T}"/> with MongoDB in chain.</returns>
+    public static IResourceBuilder<TDestination> WithMongo<TDestination>(
+        this IResourceBuilder<TDestination> resourceBuilder,
+        IDistributedApplicationBuilder appBuilder)
+        where TDestination : IResourceWithEnvironment
     {
-        var mongoUsername = builder.AddParameter("mongo-username");
-        var mongoPassword = builder.AddParameter("mongo-password", secret: true);
+        var mongoUsername = appBuilder.AddParameter("mongo-username");
+        var mongoPassword = appBuilder.AddParameter("mongo-password", secret: true);
 
-        var mongo = builder
+        var mongo = appBuilder
             .AddMongoDB("openkoqis-mongo", userName: mongoUsername, password: mongoPassword)
             // Solved error: "MongoDB cannot start: Linux kernel versions 6.19 and newer has a known incompatibility with this version of MongoDB."
             // Remove when mongodb official image will migrate to the linux 7.1+ base image.
             .WithEnvironment("GLIBC_TUNABLES", "glibc.pthread.rseq=1")
             .WithDataVolume()
-            .WithLifetime(ContainerLifetime.Persistent);
+            .WithLifetime(ContainerLifetime.Persistent)
+            .AddDatabase("openkoqis");
 
-        // Split into 1 DB per OpenKoqis Module in the future. If some module will require the mongo. It maybe even deleted in the future.
-        return mongo.AddDatabase("openkoqis");
+        return resourceBuilder.WithReference(mongo);
     }
 }
