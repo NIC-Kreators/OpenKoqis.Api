9019c39fd65a83d5fb5f30b4f9893c38bbdafb0f
Andrey Kirik <dev.andrey.kirik@gmail.com>
2026-09-18 17:00:14 +0500

    docs: removed redundant comments

--- src/OpenKoqis.Api.AppHost/AppHost.cs
+++ src/OpenKoqis.Api.AppHost/AppHost.cs
@@ -2,16 +2,9 @@ using OpenKoqis.Api.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// General Mongo
 var mongoDb = Mongo.GetMongo(builder);
-
-// PostgreSQL
 var postgres = Postgres.GetPostgres(builder);
-
-// Message Bus with IoT devices (mqtt via eclipse-mosquitto)
 var messageBusEndpoint = MessageBus.GetMessageBus(builder);
-
-// Keycloak
 var keycloak = builder.AddKeycloak("keycloak");
 
 builder.AddProject<Projects.OpenKoqis_Api>("openkoqis-api")
