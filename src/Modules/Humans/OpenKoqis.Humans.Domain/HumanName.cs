987fa774631e1a367428799ffeebf774f382bfae
Andrey Kirik <dev.andrey.kirik@gmail.com>
2026-09-19 00:32:27 +0500

    fix: HumanName VO

--- src/Modules/Humans/OpenKoqis.Humans.Domain/HumanName.cs
+++ src/Modules/Humans/OpenKoqis.Humans.Domain/HumanName.cs
@@ -8,16 +8,22 @@ namespace OpenKoqis.Humans.Domain;
 [DebuggerDisplay("{FullName}")]
 public class HumanName : ValueObject
 {
-    public required string FirstName { get; init; }
-    public string? LastName { get; init; }
-    public string? MiddleName { get; init; }
+    public string FirstName { get; }
+    public string? LastName { get; private init; }
+    public string? MiddleName { get; private init; }
 
     public string FullName => $"{FirstName} {LastName} {MiddleName}".Trim();
 
-    private HumanName() { }
+    private HumanName(string firstName)
+    {
+        FirstName = firstName;
+    }
 
     public static ErrorOr<HumanName> Parse(string raw)
     {
+        if (string.IsNullOrWhiteSpace(raw))
+            return HumanNameErrors.WrongAmountOfWords(0);
+
         var words = raw.Split(' ');
 
         if (words.Length is < 1 or > 3)
@@ -33,9 +39,8 @@ public class HumanName : ValueObject
             words[i] = finalWord.Value;
         }
 
-        return new HumanName
+        return new HumanName(words[0])
         {
-            FirstName = words[0],
             LastName = words.Length >= 2 ? words[1] : null,
             MiddleName = words.Length >= 3 ? words[2] : null,
         };
