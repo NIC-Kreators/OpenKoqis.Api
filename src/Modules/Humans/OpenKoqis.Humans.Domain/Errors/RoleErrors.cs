e355d297a91adba41e7fc36cf7c6ae9812275047
Andrey Kirik <dev.andrey.kirik@gmail.com>
2026-09-24 19:01:56 +0500

    feat: add Role entity

---
+++ src/Modules/Humans/OpenKoqis.Humans.Domain/Errors/RoleErrors.cs
@@ -0,0 +1,18 @@
+using ErrorOr;
+
+namespace OpenKoqis.Humans.Domain.Errors;
+
+public static class RoleErrors
+{
+    public static Error InvalidName => Error.Validation(
+        code: "Role.InvalidName",
+        description: "Name should consist with letters and be not larger 16 symbols.");
+
+    public static Error AtLeastOneAccessShouldBeDefined => Error.Validation(
+        code: "Role.AtLeastOneAccessShouldBeDefined",
+        description: "You cannot create a role without any access.");
+
+    public static Error CouldntDetermineCreator => Error.Validation(
+        code: "Role.CouldntDetermineCreator",
+        description: "Creator cannot be empty.");
+}
---
+++ src/Modules/Humans/OpenKoqis.Humans.Domain/Role.cs
@@ -0,0 +1,85 @@
+using System.Diagnostics;
+using ErrorOr;
+using OpenKoqis.Humans.Domain.Errors;
+using OpenKoqis.Shared.Kernel;
+using OpenKoqis.Shared.Kernel.Rules;
+
+namespace OpenKoqis.Humans.Domain;
+
+[DebuggerDisplay("{Name}")]
+public class Role : Entity<Guid>
+{
+    public record CreationAttributes
+    {
+        public required string Name { get; init; }
+        public required IEnumerable<Access> Accesses { get; init; }
+        public required Guid CreatedBy { get; init; }
+    }
+
+    private HashSet<Access> _accesses = [];
+
+    public string Name { get; private set; } = null!;
+    public IReadOnlySet<Access> Accesses => _accesses;
+    public required Guid CreatedBy { get; init; }
+
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+
+    private Role(Guid id) : base(id) { }
+
+    public static ErrorOr<Role> Create(CreationAttributes attributes)
+    {
+        if (attributes.CreatedBy == Guid.Empty)
+            return RoleErrors.CouldntDetermineCreator;
+
+        var trimmed = attributes.Name.Trim();
+
+        if (!NameRules.IsValid(trimmed))
+            return RoleErrors.InvalidName;
+
+        var accessSet = attributes.Accesses as HashSet<Access> ?? [.. attributes.Accesses];
+
+        if (accessSet.Count == 0)
+            return RoleErrors.AtLeastOneAccessShouldBeDefined;
+
+        return new Role(Guid.CreateVersion7())
+        {
+            Name = trimmed,
+            CreatedBy = attributes.CreatedBy,
+            _accesses = accessSet
+        };
+    }
+
+    public ErrorOr<Updated> Rename(string name)
+    {
+        var trimmed = name.Trim();
+
+        if (!NameRules.IsValid(trimmed))
+            return RoleErrors.InvalidName;
+
+        Name = name;
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Updated;
+    }
+
+    public ErrorOr<Updated> GrantAccess(IEnumerable<Access> accesses)
+    {
+        foreach (var access in accesses)
+            _accesses.Add(access);
+
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Updated;
+    }
+
+    public ErrorOr<Updated> RevokeAccess(IEnumerable<Access> accesses)
+    {
+        foreach (var access in accesses)
+            _accesses.Remove(access);
+
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Updated;
+    }
+}
